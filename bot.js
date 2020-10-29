614bb453093303cb3b48c5b04fe287207cfd53d4
Ai-magic <69359273+Ai-magic@users.noreply.github.com>
2020-10-29 17:02:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,46 +17,46 @@ client.once('ready', async () =>{
 });
 var id=null;
 client.on('message', async message=>{
-        const multiplyer=Math.ceil(await keyv.get('multiplyer')/2);
-        const multiplyerCost=await keyv.get('multiplyerCost');
-        var buy=[1];
-        var shop="```asciidoc\nShop\n================\n2x Multiplyer     ("+multiplyer+")::              "+multiplyerCost+" Number ```"
-        if(!message.content.startsWith(prefix) || message.author.bot){
+    const multiplyer=Math.ceil(await keyv.get('multiplyer')/2);
+    const multiplyerCost=await keyv.get('multiplyerCost');
+    var buy=[1];
+    var shop="```asciidoc\nShop\n================\n2x Multiplyer     ("+multiplyer+")::              "+multiplyerCost+" Number ```"
+    if(!message.content.startsWith(prefix) || message.author.bot){
+        return;
+    }
+    const args=message.content.slice(prefix.length).split(/ +/);
+    const command= args.shift().toLowerCase();
+    if(command=="shop"){
+        message.channel.send(shop+" ");
+        return;
+    }else if(command==")buy 1"){
+        if(await keyv.get('multyplierCost')<=await keyv.get('num')){
+            await keyv.set('multiplyer',await keyv.get('multiplyer')*2);
+            await keyv.set('num',await keyv.get('num')-await keyv.get('multiplyerCost'));
+            await keyv.set('multiplyerCost',await keyv.get('multiplyerCost')*4);
+            message.channel.send("Multiplyer: "+await keyv.get('multiplyer')+"x    Number: "+await keyv.get('num'));
+            return;
+        }else{
+            message.channel.send("You are too poor Get more $$$$$");
             return;
         }
-        const args=message.content.slice(prefix.length).split(/ +/);
-        const command= args.shift().toLowerCase();
-        if(command=="shop"){
-            message.channel.send(shop+" ");
-            return;
-        }else if(command==")buy 1"){
-            if(await keyv.get('multyplierCost')<=await keyv.get('num')){
-                await keyv.set('multiplyer',await keyv.get('multiplyer')*2);
-                await keyv.set('num',await keyv.get('num')-await keyv.get('multiplyerCost'));
-                await keyv.set('multiplyerCost',await keyv.get('multiplyerCost')*4);
-                message.channel.send("Multiplyer: "+await keyv.get('multiplyer')+"x    Number: "+await keyv.get('num'));
-                return;
-            }else{
-                message.channel.send("You are too poor Get more $$$$$");
-                return;
-            }
-        }else if(command=="number"){
+    }else if(command=="number"){
             message.channel.send(await keyv.get('num')+" "+await keyv.get('multiplyer'));
             return;
-        }
-        //id===null || message.author.id!=id ||
-        /*}else if(id==message.author.id){
-            message.delete();
-            message.channel.send("NO DOUBLE POSTING");
-        }*/
-        if( (parseInt(command))){
-                if(parseInt(command)==await keyv.get('num')+(await keyv.get('multiplyer'))){
-                        await keyv.set('num',await keyv.get('num')+(await keyv.get('multiplyer')));
-                        return;
-                }else{
-                    message.delete();
-                    message.channel.send("Next number is "+(await keyv.get('num'))+(await keyv.get('multiplyer'))+" not "+message);
-                }
-        }
+    }
+    //id===null || message.author.id!=id ||
+    /*}else if(id==message.author.id){
+        message.delete();
+        message.channel.send("NO DOUBLE POSTING");
+    }*/
+    if( (parseInt(command))){
+            if(parseInt(command)==await keyv.get('num')+(await keyv.get('multiplyer'))){
+                    await keyv.set('num',await keyv.get('num')+(await keyv.get('multiplyer')));
+                    return;
+            }else{
+                message.delete();
+                message.channel.send("Next number is "+(await keyv.get('num'))+(await keyv.get('multiplyer'))+" not "+message);
+            }
+    }
         id=message.author.id;
-    });
+});
